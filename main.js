b0e79065f9ad78428275f94de2169e685589a216
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-07-15 13:37:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let body = document.querySelector('body')
+
+setInterval(() => {
+    let p = document.createElement('p')
+
+    let random = Math.ceil(Math.random() * 100)
+    p.innerHTML = random
+
+    random = textcontent 
+    body.append(p)
+}, 1000);
